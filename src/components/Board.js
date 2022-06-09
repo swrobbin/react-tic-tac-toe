5e8c1bb8c373a5be8a067d598182af97aa2fcd8e
Stephen Robbins <swrobbin@gmail.com>
2022-06-09 13:18:39 -0400

    Board compenent

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import Square from './Square'
 
-const Board = () => {
+const Board = ({ squares, onClick}) => {
+
+
   return (
-    <div>Board</div>
+    <div>
+        <Square value="1" onClick={() => onClick("dummy val")} />
+        <Square value="2" onClick={() => onClick("dummy val")} />
+        <Square value="3" onClick={() => onClick("dummy val")} />
+        <Square value="4" onClick={() => onClick("dummy val")} />
+        <Square value="5" onClick={() => onClick("dummy val")} />
+        <Square value="6" onClick={() => onClick("dummy val")} />
+        <Square value="7" onClick={() => onClick("dummy val")} />
+        <Square value="8" onClick={() => onClick("dummy val")} />
+        <Square value="9" onClick={() => onClick("dummy val")} /> 
+
+    </div>
   )
 }
 
