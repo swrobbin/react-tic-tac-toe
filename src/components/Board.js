0f6f5f325ec475c6c8fe8372c6b9b05b68fc8cfd
Stephen Robbins <swrobbin@gmail.com>
2022-06-13 11:52:24 -0400

    dynamically rendering squaresi in board comp

--- src/components/Board.js
+++ src/components/Board.js
@@ -16,17 +16,10 @@ const style = {
 
 
   return (
-    <div style={style} >
-        <Square value="1" onClick={() => onClick("dummy val")} />
-        <Square value="2" onClick={() => onClick("dummy val")} />
-        <Square value="3" onClick={() => onClick("dummy val")} />
-        <Square value="4" onClick={() => onClick("dummy val")} />
-        <Square value="5" onClick={() => onClick("dummy val")} />
-        <Square value="6" onClick={() => onClick("dummy val")} />
-        <Square value="7" onClick={() => onClick("dummy val")} />
-        <Square value="8" onClick={() => onClick("dummy val")} />
-        <Square value="9" onClick={() => onClick("dummy val")} /> 
-
+    <div style={style}> 
+        {squares.map((square, i) => {
+       return  <Square key={i} value={square} onClick={() => onClick("dummy value")} />
+        })}
     </div>
   )
 }
